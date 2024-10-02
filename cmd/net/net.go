42ebadfea01ba270b7492d6447596c5d75a38261
Duckduckgot <49223254+Duckduckgot@users.noreply.github.com>
2024-10-02 23:20:11 +0900

    Update net.go

--- cmd/net/net.go
+++ cmd/net/net.go
@@ -2,8 +2,8 @@ package main
 
 import (
    "flag"
-   "github.com/89z/rosso/http"
-   "github.com/89z/rosso/strconv"
+   "github.com/Duckduckgot/rosso/http"
+   "github.com/Duckduckgot/rosso/strconv"
    "net/http/httputil"
    "os"
 )
